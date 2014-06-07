dddb7953d46953f6dbc6af39011efe60e49cb6ad
Anton Karamanov <ataraxer@gmail.com>
2014-06-07 11:58:26 +0400

    Add publishing configuration

--- project/Build.scala
+++ project/Build.scala
@@ -44,6 +44,30 @@ object AkkitBuild extends Build {
         exclude("org.slf4j", "slf4j-simple")
     ),
 
+    publishTo <<= version { (ver: String) =>
+      val nexus = "http://nexus.ataraxer.com/nexus/"
+      if (ver.trim.endsWith("SNAPSHOT")) {
+        Some("Snapshots" at nexus + "content/repositories/snapshots")
+      } else {
+        Some("Releases"  at nexus + "content/repositories/releases")
+      }
+    },
+
+    publishArtifact in Test := false,
+
+    pomExtra := (
+      <scm>
+        <url>git@github.com:ataraxer/akkit.git</url>
+        <connection>scm:git:git@github.com:ataraxer/akkit.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>ataraxer</id>
+          <name>Anton Karamanov</name>
+          <url>github.com/ataraxer</url>
+        </developer>
+      </developers>),
+
     gitRemoteRepo := "git@github.com:ataraxer/akkit.git",
 
     parallelExecution := true
@@ -58,7 +82,11 @@ object AkkitBuild extends Build {
     ScoverageSbtPlugin.instrumentSettings ++
     Seq(
       name         := "akkit",
-      version      := "0.1.0",
+      homepage     := Some(url("http://github.com/ataraxer/akkit")),
+      licenses     := Seq("MIT License" -> url(
+        "http://www.opensource.org/licenses/mit-license.php"
+      )),
+      version      := "0.1.1-SNAPSHOT",
       scalaVersion := "2.10.3"
     )
 
